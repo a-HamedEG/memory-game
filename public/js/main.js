2d82fc66592c82feed183130a1c912db75680492
Ahmed Hamed <111521604+a-HamedEG@users.noreply.github.com>
2022-11-09 01:51:53 +0200

    Adding Comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,38 @@
 import "../css/style.css";
-
-const cardWrapper = document.querySelector('.card-wrapper');
-const wrongTries = document.querySelector('.wrong-tries span');
-
+const cardWrapper = document.querySelector(".card-wrapper");
+const wrongTries = document.querySelector(".wrong-tries span");
 const cards = Array.from(document.querySelectorAll(".card"));
-
 const numberOfCardsArray = Array.from(Array(cards.length).keys());
-
 const duration = 1000;
 
 document.querySelector(".start-btn button").onclick = function () {
+  // Store Player Name
   const name = prompt("What's your name");
-
+  // Check if name exists
   if (name == null || name == "") {
     document.querySelector(".name span").innerHTML = "Player";
   } else {
     document.querySelector(".name span").innerHTML = name;
   }
-
-document.querySelector(".start-btn").remove();
+  // remove start Game Overlay
+  document.querySelector(".start-btn").remove();
 };
-
+// Shuffle Array Numbers to Get a Random order
 shuffle(numberOfCardsArray);
 
 document.querySelectorAll(".card").forEach((card, index) => {
+  // Shuffle Cards location
   card.style.order = numberOfCardsArray[index];
+  // Add onClick Event
   card.addEventListener("click", () => flipCard(card));
 });
 
 function shuffle(array) {
+  // Simply Change the index of the Array
+  // 1- Stores the last index value in a -Temp- Variable
+  // 2- Replace that index value with 'random' index value in same array
+  // 3- change that 'random' value with the -Temp-
+
   let current = array.length,
     temp,
     random;
@@ -36,42 +40,46 @@ function shuffle(array) {
   while (current > 0) {
     random = Math.floor(Math.random() * current);
     current--;
+    // 1- Stores the last index value in a -Temp- Variable
     temp = array[current];
-
+    // 2- Replace that index value with 'random' index value in same array
     array[current] = array[random];
-
+    // 3- change that 'random' value with the -Temp-
     array[random] = temp;
   }
 }
 
 function flipCard(card) {
   card.classList.add("is-flipped");
+  // Checking if Two Cards is Selected to Check thier Match
   if (document.querySelectorAll(".is-flipped").length == 2) {
-    preventClickingOnCards()
-    let firstPickCard = document.querySelectorAll(".is-flipped")[0]
-    let secondPickCard = document.querySelectorAll(".is-flipped")[1]
-    MatchCheck(firstPickCard,secondPickCard);
-    setTimeOutFor(secondPickCard,'is-flipped')
-    setTimeOutFor(firstPickCard,'is-flipped')
+    // prevent any Clicking on Cards Wrapper For better Experience
+    preventClickingOnCards();
+    let firstPickCard = document.querySelectorAll(".is-flipped")[0];
+    let secondPickCard = document.querySelectorAll(".is-flipped")[1];
+    // Check thier Match
+    MatchCheck(firstPickCard, secondPickCard);
+    setTimeOutFor(secondPickCard, "is-flipped");
+    setTimeOutFor(firstPickCard, "is-flipped");
   }
 }
 
 function MatchCheck(firstPickCard, secondPickCard) {
-    if (firstPickCard.dataset.techology === secondPickCard.dataset.techology) {
-        firstPickCard.classList.add("match")
-        secondPickCard.classList.add("match")
-    } else {
-        wrongTries.innerHTML = parseInt(wrongTries.innerHTML) + 1;
-    }
+  if (firstPickCard.dataset.techology === secondPickCard.dataset.techology) {
+    firstPickCard.classList.add("match");
+    secondPickCard.classList.add("match");
+  } else {
+    wrongTries.innerHTML = parseInt(wrongTries.innerHTML) + 1;
+  }
 }
 
 function preventClickingOnCards() {
-    cardWrapper.classList.add('no-clicking');
-    setTimeOutFor(cardWrapper,'no-clicking')
+  cardWrapper.classList.add("no-clicking");
+  setTimeOutFor(cardWrapper, "no-clicking");
 }
 
 function setTimeOutFor(element, classname) {
-    setTimeout(() => {
-        element.classList.remove(classname);
-    }, duration);
+  setTimeout(() => {
+    element.classList.remove(classname);
+  }, duration);
 }
