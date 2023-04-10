9d8ca08474f7957f603d9f7ccba827ecd52d4e1e
Ahmed Hamed <111521604+a-HamedEG@users.noreply.github.com>
2023-04-10 18:11:19 +0200

    Generating Cards using JS

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,12 @@
 import "../css/style.css";
 const cardWrapper = document.querySelector(".card-wrapper");
 const wrongTries = document.querySelector(".wrong-tries span");
-const cards = Array.from(document.querySelectorAll(".card"));
-const numberOfCardsArray = Array.from(Array(cards.length).keys());
 const winningSound = document.querySelector(".niceTry");
 const losingSound = document.querySelector(".tryAgain");
+
 // Game Variables
 const duration = 1000;
-const timeInSecond = 60;
+const timeInSecond = 120;
 
 class Timer {
   constructor(root, remainingSeconds) {
@@ -58,6 +57,107 @@ class Timer {
   }
 }
 
+const cardsdata = [{
+  cardName : "code",
+  imgName : "code.svg"
+},{
+  cardName : "css",
+  imgName : "css.svg"
+},{
+  cardName : "html",
+  imgName : "html.svg"
+},{
+  cardName : "php",
+  imgName : "php.svg"
+},{
+  cardName : "nodejs",
+  imgName : "nodejs.svg"
+},{
+  cardName : "react",
+  imgName : "react.svg"
+},{
+  cardName : "vuejs",
+  imgName : "vuejs.svg"
+},{
+  cardName : "wordpress",
+  imgName : "wordpress.svg"
+},{
+  cardName : "laravel",
+  imgName : "laravel.svg"
+},{
+  cardName : "javascript",
+  imgName : "javascript.svg"
+}]
+
+async function shuffle(array) {
+  // Simply Change the index of the Array
+  // 1- Stores the last index value in a -Temp- Variable
+  // 2- Replace that index value with 'random' index value in same array
+  // 3- change that 'random' value with the -Temp-
+
+  let current = array.length,
+    temp,
+    random;
+
+  while (current > 0) {
+    console.log(current)
+    random = Math.floor(Math.random() * current);
+    current--;
+    // 1- Stores the last index value in a -Temp- Variable
+    temp = array[current];
+    // 2- Replace that index value with 'random' index value in same array
+    array[current] = array[random];
+    // 3- change that 'random' value with the -Temp-
+    array[random] = temp;
+  }
+}
+
+const load = async () => {
+  await generateCards(cardsdata)
+
+  const cards = Array.from(document.querySelectorAll(".card"));
+  const numberOfCardsArray = Array.from(Array(cards.length).keys());
+
+  // Shuffle Array Numbers to Get a Random order
+  shuffle(numberOfCardsArray);
+
+  document.querySelectorAll(".card").forEach((card, index) => {
+    // Shuffle Cards location
+    card.style.order = numberOfCardsArray[index];
+    // Add onClick Event
+    card.addEventListener("click", () => flipCard(card));
+  });
+}
+
+document.addEventListener('load', load())
+
+// generate html for Cards
+async function generateCards(cardsdata) {
+  let cardsHtml='';
+  for (let i = 0; i < cardsdata.length; i++) {
+    const card = 
+    `<div class="card" data-techology="${cardsdata[i].cardName}">
+        <div class="face front"></div>
+        <div class="face back">
+          <img src="./public/imgs/${cardsdata[i].imgName}" alt="${cardsdata[i].cardName}">
+        </div>
+      </div>
+
+      <div class="card" data-techology="${cardsdata[i].cardName}">
+        <div class="face front"></div>
+        <div class="face back">
+          <img src="./public/imgs/${cardsdata[i].imgName}" alt="${cardsdata[i].cardName}">
+        </div>
+      </div>
+    `;
+    cardsHtml += card
+  }
+  cardWrapper.innerHTML = cardsHtml
+}
+
+
+// Example usage:
+
 document.querySelector(".start-btn button").onclick = function () {
   // Store Player Name
   const name = prompt("What's your name");
@@ -75,40 +175,13 @@ document.querySelector(".start-btn button").onclick = function () {
 document.querySelector(".game-over button").onclick = function () {
   location.reload();
 }
-// Shuffle Array Numbers to Get a Random order
-shuffle(numberOfCardsArray);
 
-document.querySelectorAll(".card").forEach((card, index) => {
-  // Shuffle Cards location
-  card.style.order = numberOfCardsArray[index];
-  // Add onClick Event
-  card.addEventListener("click", () => flipCard(card));
-});
 
-function shuffle(array) {
-  // Simply Change the index of the Array
-  // 1- Stores the last index value in a -Temp- Variable
-  // 2- Replace that index value with 'random' index value in same array
-  // 3- change that 'random' value with the -Temp-
 
-  let current = array.length,
-    temp,
-    random;
-
-  while (current > 0) {
-    random = Math.floor(Math.random() * current);
-    current--;
-    // 1- Stores the last index value in a -Temp- Variable
-    temp = array[current];
-    // 2- Replace that index value with 'random' index value in same array
-    array[current] = array[random];
-    // 3- change that 'random' value with the -Temp-
-    array[random] = temp;
-  }
-}
 
 function flipCard(card) {
   card.classList.add("is-flipped");
+  
   // Checking if Two Cards is Selected to Check thier Match
   if (document.querySelectorAll(".is-flipped").length == 2) {
     // prevent any Clicking on Cards Wrapper For better Experience
